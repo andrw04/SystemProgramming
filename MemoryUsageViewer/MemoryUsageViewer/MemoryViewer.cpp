bd0c5ad51c1a2053c415892206f99e14e1455978
andrw04 <sivyandrey2021@gmail.com>
2023-10-11 13:07:37 +0300

    lab 4 completed

--- MemoryUsageViewer/MemoryUsageViewer/MemoryViewer.cpp
+++ MemoryUsageViewer/MemoryUsageViewer/MemoryViewer.cpp
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <string>
+#include <TlHelp32.h>
 #include "MemoryViewer.h"
+#include <windows.h>
+#include <tlhelp32.h>
+
 
 int UpdateProcessList() {
 	DWORD processes[1024];
@@ -66,6 +70,30 @@ int UpdateProcessList() {
 
 				swprintf_s(buffer, L"% .3lf", (double)pmc.WorkingSetSize / (1024 * 1024));
 				ListView_SetItemText(hList, row, 2, buffer);
+
+				DWORD exitCode;
+				if (GetExitCodeProcess(hProcess, &exitCode))
+				{
+					if (exitCode == STILL_ACTIVE)
+					{
+						length = strlen(path) + 1;
+						size = MultiByteToWideChar(CP_UTF8, 0, "Active", length, NULL, 0);
+						MultiByteToWideChar(CP_UTF8, 0, "Active", length, buffer, size);
+					}
+					else {
+						// Процесс приостановлен
+						length = strlen(path) + 1;
+						size = MultiByteToWideChar(CP_UTF8, 0, "Suspended", length, NULL, 0);
+						MultiByteToWideChar(CP_UTF8, 0, "Suspended", length, buffer, size);
+					}
+				}
+				else {
+					length = strlen(path) + 1;
+					size = MultiByteToWideChar(CP_UTF8, 0, "Undefined", length, NULL, 0);
+					MultiByteToWideChar(CP_UTF8, 0, "Undefined", length, buffer, size);
+				}
+
+				ListView_SetItemText(hList, row, 3, buffer);
 
 				row++;
 				std::cout << "Process Identificator: " << processes[i] << " Process Name: " << path << " Memory usage: " << std::fixed << std::setprecision(2) << (double)pmc.WorkingSetSize / (1024 * 1024) << " Mb" << std::endl;
@@ -128,8 +156,203 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	return 0;
 }
 
+void CreateNewProcess() {
+	OPENFILENAME ofn;
+	wchar_t filePath[MAX_PATH] = L"";
+
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = L"Executable Files (*.exe)\0*.exe\0All Files (*.*)\0*.*\0";
+	ofn.lpstrFile = filePath;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+
+	if (GetOpenFileName(&ofn)) {
+		STARTUPINFO si = { 0 };
+		PROCESS_INFORMATION pi = { 0 };
+
+		if (CreateProcess(NULL, filePath, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+			CloseHandle(pi.hProcess);
+			CloseHandle(pi.hThread);
+		}
+		else {
+			MessageBox(NULL, L"Failed to start process!", L"Error", MB_OK | MB_ICONERROR);
+		}
+	}
+}
+
+void SuspendProcess() {
+	// Получаем индекс выбранной строки
+	int iSelectedCount = ListView_GetSelectedCount(hList);
+
+	if (iSelectedCount > 0)
+	{
+		int selectedIndex = ListView_GetSelectionMark(hList);
+
+		// Выделям память под структуру для хранения информации о ячейке
+		LVITEM item;
+		item.mask = LVIF_TEXT;
+		item.iItem = selectedIndex;
+		item.iSubItem = 0;
+		item.pszText = new wchar_t[MAX_PATH];
+		item.cchTextMax = MAX_PATH;
+
+		// Получаем значение выбранной ячейки
+		ListView_GetItemText(hList, selectedIndex, 0, item.pszText, MAX_PATH);
+
+		DWORD processId = wcstoul(item.pszText, NULL, 10);
+
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+		if (hProcess == NULL)
+		{
+			MessageBox(hwnd, L"Can't open process.", L"Error", MB_OK | MB_ICONERROR);
+			return;
+		}
+
+		// Приостановка процесса
+		DWORD threadCount = 0;
+		HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+		THREADENTRY32 te32;
+		te32.dwSize = sizeof(THREADENTRY32);
+
+		if (Thread32First(hThreadSnapshot, &te32))
+		{
+			do
+			{
+				if (te32.th32OwnerProcessID == GetProcessId(hProcess))
+				{
+					HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te32.th32ThreadID);
+					if (hThread != NULL)
+					{
+						SuspendThread(hThread);
+						CloseHandle(hThread);
+						threadCount++;
+					}
+				}
+			} while (Thread32Next(hThreadSnapshot, &te32));
+		}
+		MessageBox(hwnd, L"Process suspended!", L"Info", MB_OK | MB_ICONINFORMATION);
+
+		CloseHandle(hThreadSnapshot);
+
+		delete[] item.pszText;
+	}
+	else {
+		MessageBox(hwnd, L"Element not selected!", L"Error", MB_OK | MB_ICONERROR);
+	}
+}
+
+
+
+void ResumeProcess() {
+	// Получаем индекс выбранной строки
+	int iSelectedCount = ListView_GetSelectedCount(hList);
+
+	if (iSelectedCount > 0)
+	{
+		int selectedIndex = ListView_GetSelectionMark(hList);
+
+		// Выделям память под структуру для хранения информации о ячейке
+		LVITEM item;
+		item.mask = LVIF_TEXT;
+		item.iItem = selectedIndex;
+		item.iSubItem = 0;
+		item.pszText = new wchar_t[MAX_PATH];
+		item.cchTextMax = MAX_PATH;
+
+		// Получаем значение выбранной ячейки
+		ListView_GetItemText(hList, selectedIndex, 0, item.pszText, MAX_PATH);
+
+		DWORD processId = wcstoul(item.pszText, NULL, 10);
+
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+		if (hProcess == NULL)
+		{
+			MessageBox(hwnd, L"Can't open process.", L"Error", MB_OK | MB_ICONERROR);
+			return;
+		}
+
+		// Возобновление процесса
+		DWORD threadCount = 0;
+		HANDLE hThreadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+		THREADENTRY32 te32;
+		te32.dwSize = sizeof(THREADENTRY32);
+
+		if (Thread32First(hThreadSnapshot, &te32)) {
+			do {
+				if (te32.th32OwnerProcessID == GetProcessId(hProcess)) {
+					HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te32.th32ThreadID);
+					if (hThread != NULL) {
+						ResumeThread(hThread);
+						CloseHandle(hThread);
+					}
+				}
+			} while (Thread32Next(hThreadSnapshot, &te32));
+		}
+
+		MessageBox(hwnd, L"Process resumed!", L"Info", MB_OK | MB_ICONINFORMATION);
+
+		CloseHandle(hThreadSnapshot);
+		CloseHandle(hProcess);
+
+		delete[] item.pszText;
+	}
+	else {
+		MessageBox(hwnd, L"Element not selected!", L"Error", MB_OK | MB_ICONERROR);
+	}
+}
+
+void TerminateProcess() {
+	int iSelectedCount = ListView_GetSelectedCount(hList);
+
+	if (iSelectedCount > 0)
+	{
+		int selectedIndex = ListView_GetSelectionMark(hList);
+
+		// Выделям память под структуру для хранения информации о ячейке
+		LVITEM item;
+		item.mask = LVIF_TEXT;
+		item.iItem = selectedIndex;
+		item.iSubItem = 0;
+		item.pszText = new wchar_t[MAX_PATH];
+		item.cchTextMax = MAX_PATH;
+
+		// Получаем значение выбранной ячейки
+		ListView_GetItemText(hList, selectedIndex, 0, item.pszText, MAX_PATH);
+
+		DWORD processId = wcstoul(item.pszText, NULL, 10);
+
+		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+		if (hProcess == NULL)
+		{
+			MessageBox(hwnd, L"Can't open process.", L"Error", MB_OK | MB_ICONERROR);
+			return;
+		}
+
+		// Закрытие процесса
+		TerminateProcess(hProcess, 0);
+
+		CloseHandle(hProcess);
+	}
+	else {
+		MessageBox(hwnd, L"Element not selected!", L"Error", MB_OK | MB_ICONERROR);
+	}
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
+	case WM_COMMAND:
+		if (lParam == (LPARAM)createButton)
+			CreateNewProcess();
+		else if (lParam == (LPARAM)suspendButton)
+			SuspendProcess();
+		else if (lParam == (LPARAM)resumeButton)
+			ResumeProcess();
+		else if (lParam == (LPARAM)terminateButton)
+			TerminateProcess();
+		break;
+
 	case WM_SIZE:
 	{
 		break;
@@ -152,7 +375,12 @@ VOID CALLBACK TimerCallback(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime
 
 void CreateMainWindow() {
 	hwnd = CreateWindow(_T("MyWindowClass"), L"MemoryViewer",WS_OVERLAPPEDWINDOW, 100, 100, 600, 700, NULL, NULL, hInstance, NULL);
-	hList = CreateWindow(WC_LISTVIEW, _T(""), WS_CHILD | WS_VISIBLE | LVS_REPORT, 0, 0, 585, 700, hwnd, NULL, NULL, NULL);
+	hList = CreateWindow(WC_LISTVIEW, _T(""), WS_CHILD | WS_VISIBLE | LVS_REPORT, 0, 40, 585, 620, hwnd, NULL, NULL, NULL);
+
+	createButton = CreateWindowEx(0, L"BUTTON", L"Create", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 5, 5, 100, 30, hwnd, NULL, hInstance, NULL);
+	suspendButton = CreateWindowEx(0, L"BUTTON", L"Suspend", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 110, 5, 100, 30, hwnd, NULL, hInstance, NULL);
+	resumeButton = CreateWindowEx(0, L"BUTTON", L"Resume", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 215, 5, 100, 30, hwnd, NULL, hInstance, NULL);
+	terminateButton = CreateWindowEx(0, L"BUTTON", L"Terminate", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 320, 5, 100, 30, hwnd, NULL, hInstance, NULL);
 
 	ListView_SetExtendedListViewStyle(hList, LVS_EX_FULLROWSELECT);
 
@@ -170,7 +398,13 @@ void CreateMainWindow() {
 
 	LVCOLUMN lvColumn3;
 	lvColumn3.mask = LVCF_TEXT | LVCF_WIDTH;
-	lvColumn3.cx = 200;
+	lvColumn3.cx = 150;
 	lvColumn3.pszText = const_cast<wchar_t*>(L"Memory Usage (Mb)");
 	ListView_InsertColumn(hList, 2, &lvColumn3);
+
+	LVCOLUMN lvColumn4;
+	lvColumn4.mask = LVCF_TEXT | LVCF_WIDTH;
+	lvColumn4.cx = 50;
+	lvColumn4.pszText = const_cast<wchar_t*>(L"Status");
+	ListView_InsertColumn(hList, 3, &lvColumn4);
 }
--- MemoryUsageViewer/MemoryUsageViewer/MemoryViewer.h
+++ MemoryUsageViewer/MemoryUsageViewer/MemoryViewer.h
@@ -3,11 +3,21 @@
 #include <tchar.h>
 #include "commctrl.h"
 
+#define OnCreateClicked 2
+#define OnSuspendClicked 3
+#define OnResumeClicked 4
+#define OnTerminateClicked 5
+
 HINSTANCE hInstance;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void CreateMainWindow();
 HWND hwnd;
 HWND hList;
+
+HWND createButton;
+HWND suspendButton;
+HWND resumeButton;
+HWND terminateButton;
 
 DWORD interval = 1000;
